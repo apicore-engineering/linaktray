79387d5c528b208dee88bc055bf4551b2cf2fae6
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2022-10-04 15:38:54 +0200

    Add gtk implementation

--- LinakTray.py
+++ LinakTray.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """Small system tray application to adjust height of a Linak-based desk"""
-from linak_tray_qt import LinakTray
+import os
+
+if os.environ.get("DESKTOP_SESSION") == "ubuntu":
+    from linak_tray_gtk import LinakTray
+else:
+    from linak_tray_qt import LinakTray
 
 LinakTray(config='linaktray.conf').run()
---
+++ linak_tray_gtk.py
@@ -0,0 +1,83 @@
+"""Small system tray application to adjust height of a Linak-based desk"""
+# pylint: disable=wrong-import-position,wrong-import-order
+import os
+import sys
+import gi
+from linak_controller import LinakController
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('AyatanaAppIndicator3', '0.1')
+from gi.repository import AyatanaAppIndicator3 as appindicator
+from gi.repository import Gtk as gtk
+
+class LinakTray(LinakController):
+    """System tray icon class"""
+    def __init__(self, config=None):
+        self.indicator = self._create_indicator()
+
+        super().__init__(config=config)
+
+        self.menu = self._construct_menu()
+        self.indicator.set_menu(self.menu)
+
+    @staticmethod
+    def run():
+        """Run the main loop"""
+        gtk.main()
+
+    @staticmethod
+    def _create_indicator():
+        indicator = appindicator.Indicator.new(
+            "LinakTray",
+            "icon",
+            appindicator.IndicatorCategory.APPLICATION_STATUS,
+        )
+        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+        return indicator
+
+    def _construct_menu(self):
+        """Construct a menu from the current configuration"""
+        menu = gtk.Menu()
+        self._construct_menu_toggle(menu)
+        self._construct_menu_separator(menu)
+        self._construct_menu_positions(menu)
+        self._construct_menu_separator(menu)
+        self._construct_menu_quit(menu)
+        menu.show()
+        return menu
+
+    def _construct_menu_positions(self, parent):
+        """Create entries for saved positions"""
+        for key, value in self.positions.items():
+            action = gtk.MenuItem(label=key)
+            action.connect("activate", lambda state, position=value: self.set_position(position))
+            action.show()
+            parent.append(action)
+
+    def _construct_menu_toggle(self, parent):
+        """Create toggle favourite button"""
+        action_toggle = gtk.MenuItem(label="Toggle")
+        action_toggle.connect("activate", lambda _: self.toggle_favourite())
+        action_toggle.show()
+        self.indicator.set_secondary_activate_target(action_toggle)
+        parent.append(action_toggle)
+
+    @classmethod
+    def _construct_menu_quit(cls, parent):
+        """Create quit button"""
+        action_quit = gtk.MenuItem(label="Quit")
+        action_quit.connect("activate", lambda _: sys.exit())
+        action_quit.show()
+        parent.append(action_quit)
+
+    @classmethod
+    def _construct_menu_separator(cls, parent):
+        """Create a separator menu item"""
+        separator = gtk.SeparatorMenuItem()
+        separator.show()
+        parent.append(separator)
+
+    def _set_icon(self, path):
+        path = self._get_absolute_path(path)
+        self.indicator.set_icon_theme_path(os.path.dirname(path))
+        self.indicator.set_icon_full(os.path.splitext(os.path.basename(path))[0], "Icon")
