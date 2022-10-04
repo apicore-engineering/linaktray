3e52dafa3b326a96d444c1b6d850dd18ec3d7ee3
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2022-10-04 17:36:43 +0200

    Respect config gui setting

--- LinakTray.py
+++ LinakTray.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Small system tray application to adjust height of a Linak-based desk"""
 import os
+from config import Config
 
-if os.environ.get("DESKTOP_SESSION") == "ubuntu":
+def _get_gui_type():
+    """Detect the GUI type to use"""
+    config = Config().get('settings', 'gui')
+    if config in ('gtk', 'qt'):
+        return config
+    if os.environ.get("DESKTOP_SESSION") in ("ubuntu", "cinnamon"):
+        return 'gtk'
+    return 'qt'
+
+if _get_gui_type() == 'gtk':
     from linak_tray_gtk import LinakTray
 else:
     from linak_tray_qt import LinakTray
